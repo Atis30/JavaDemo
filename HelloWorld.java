2d33d4f1a1e267e3c6817c261732f285afb48ebf
Atish Londhe <l.atishd@gmail.com>
2023-04-20 09:41:37 +0530

    Chnages made to the files

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,10 @@
 public class HelloWorld {
-	public static void main(String [] args) {
-	
+	public static void main(String[] args) {
+
 		System.out.println("Hello World!!!");
+
+		int i = 20;
+		System.out.println(i);
 	}
 
 }
